533717d0b2b06515a484a21ccacfd3f7446e97cc
devrajsaxena282-collab <devrajsaxena282@gmail.com>
2026-04-14 10:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def run():
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8080)),
-        debug=False
+        debug=False,
+use_reloader=False
     )
 
 def keep_alive():
